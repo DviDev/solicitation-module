d585c2e8acac2bfdfa158afb7f5756ef9b0bde18
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-20 11:58:58 -0300

    add JSON translation support to SolicitationServiceProvider

--- app/Providers/SolicitationServiceProvider.php
+++ app/Providers/SolicitationServiceProvider.php
@@ -91,8 +91,10 @@ class SolicitationServiceProvider extends ServiceProvider
 
         if (is_dir($langPath)) {
             $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
+            $this->loadJsonTranslationsFrom($langPath);
         } else {
-            $this->loadTranslationsFrom(module_path($this->moduleName, 'resources/lang'), $this->moduleNameLower);
+            $this->loadTranslationsFrom(module_path($this->moduleName, 'lang'), $this->moduleNameLower);
+            $this->loadJsonTranslationsFrom(module_path($this->moduleName, 'lang'));
         }
     }
 
