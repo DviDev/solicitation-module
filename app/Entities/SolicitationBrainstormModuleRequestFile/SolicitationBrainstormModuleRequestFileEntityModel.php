482655d2b3470285b60d96e90bf66b1c201caa3f
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-26 15:50:28 -0300

    Refactor to replace BaseEntityModel with BaseEntityModel Contract
    Replaced the usage of `Modules\Base\Entities\BaseEntityModel` with `Modules\Base\Contracts\BaseEntityModel` across multiple entity models to align with the contract-driven design approach. This ensures greater flexibility and adherence to interface-driven development practices.

--- app/Entities/SolicitationBrainstormModuleRequestFile/SolicitationBrainstormModuleRequestFileEntityModel.php
+++ app/Entities/SolicitationBrainstormModuleRequestFile/SolicitationBrainstormModuleRequestFileEntityModel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Solicitation\Entities\SolicitationBrainstormModuleRequestFile;
 
-use Modules\Base\Entities\BaseEntityModel;
+use Modules\Base\Contracts\BaseEntityModel;
 use Modules\Solicitation\Models\SolicitationBrainstormModuleRequestFileModel;
 
 /**
--- app/Entities/SolicitationBrainstormModuleRequestTask/SolicitationBrainstormModuleRequestTaskEntityModel.php
+++ app/Entities/SolicitationBrainstormModuleRequestTask/SolicitationBrainstormModuleRequestTaskEntityModel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Solicitation\Entities\SolicitationBrainstormModuleRequestTask;
 
-use Modules\Base\Entities\BaseEntityModel;
+use Modules\Base\Contracts\BaseEntityModel;
 use Modules\Solicitation\Models\SolicitationBrainstormModuleRequestTaskModel;
 
 /**
