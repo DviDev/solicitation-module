f4402b48ca7d767bcc6fcbbcf4dd5a37c5a58c6b
Davi Menezes <davimenezes.dev@gmail.com>
2026-08-07 19:48:07 -0300

    lang path improvements to phpstorm autocomplete

--- app/Providers/SolicitationServiceProvider.php
+++ app/Providers/SolicitationServiceProvider.php
@@ -4,122 +4,40 @@ declare(strict_types=1);
 
 namespace Modules\Solicitation\Providers;
 
-use Config;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\ServiceProvider;
+use Modules\Base\Contracts\BaseServiceProviderContract;
 use Modules\Base\Events\DatabaseSeederEvent;
 use Modules\Project\Events\CreateMenuItemsEvent;
 use Modules\Solicitation\Listeners\CreateMenuItemsSolicitationListener;
 use Modules\Solicitation\Listeners\SolicitationDatabaseSeederListener;
 
-final class SolicitationServiceProvider extends ServiceProvider
+final class SolicitationServiceProvider extends BaseServiceProviderContract
 {
-    /**
-     * @var string
-     */
-    protected $moduleName = 'Solicitation';
-
-    /**
-     * @var string
-     */
-    protected $moduleNameLower = 'solicitation';
-
-    /**
-     * Boot the application events.
-     *
-     * @return void
-     */
-    public function boot()
+    public function registerEvents(): void
     {
-        $this->registerTranslations();
-        $this->registerConfig();
-        $this->registerViews();
-        $this->loadMigrationsFrom(module_path($this->moduleName, 'database/Migrations'));
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->register(RouteServiceProvider::class);
-
         Event::listen(DatabaseSeederEvent::class, SolicitationDatabaseSeederListener::class);
         Event::listen(CreateMenuItemsEvent::class, CreateMenuItemsSolicitationListener::class);
     }
 
-    /**
-     * Register views.
-     *
-     * @return void
-     */
-    public function registerViews()
+    public function provides(): array
     {
-        $viewPath = resource_path('views/modules/'.$this->moduleNameLower);
-
-        $sourcePath = module_path($this->moduleName, 'resources/views');
-
-        $this->publishes([
-            $sourcePath => $viewPath,
-        ], ['views', $this->moduleNameLower.'-module-views']);
-
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
+        return [
+            RouteServiceProvider::class,
+        ];
     }
 
-    /**
-     * Register translations.
-     *
-     * @return void
-     */
-    public function registerTranslations()
+    public function getModuleName(): string
     {
-        $langPath = resource_path('lang/modules/'.$this->moduleNameLower);
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom($langPath);
-        } else {
-            $this->loadTranslationsFrom(module_path($this->moduleName, 'lang'), $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom(module_path($this->moduleName, 'lang'));
-        }
+        return 'Solicitation';
     }
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
+    public function getModuleNameLower(): string
     {
-        return [];
+        return 'solicitation';
     }
 
-    /**
-     * Register config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
+    protected function langPath(): string
     {
-        $this->publishes([
-            module_path($this->moduleName, 'config/config.php') => config_path($this->moduleNameLower.'.php'),
-        ], 'config');
-        $this->mergeConfigFrom(
-            module_path($this->moduleName, 'config/config.php'), $this->moduleNameLower
-        );
-    }
-
-    private function getPublishableViewPaths(): array
-    {
-        $paths = [];
-        foreach (Config::get('view.paths') as $path) {
-            if (is_dir($path.'/modules/'.$this->moduleNameLower)) {
-                $paths[] = $path.'/modules/'.$this->moduleNameLower;
-            }
-        }
-
-        return $paths;
+        return module_path($this->getModuleName(), 'resources/lang');
     }
 }
